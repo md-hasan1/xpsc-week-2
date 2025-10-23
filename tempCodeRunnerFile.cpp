b2df5a18268916fd9a2b215c973de474900c7423
md-hasan1 <akonhasan680@gmail.com>
2025-10-23 14:59:27 +0600

    ccc

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+    // for (auto i = 0; i < n; i++)
+    // {
+    //     cin >> m;
+    //     cout<<m<<endl;
+    //     for (auto j = 0; j < m; j++)
+    //     {
+    //         cin >>v[i][j];
+    //     }
+    //     // cout << endl;
+    // }
+    // for (auto i = 0; i < n; i++)
+    // {
+     
+    //     for (auto j = 0; j < m; j++)
+    //     {
+    //         cout<<v[i][j]<<" ";
+    //     }
+    //     cout << endl;
+    // }
--- test.cpp
+++ test.cpp
@@ -4,6 +4,22 @@ using namespace std;
 int main(){
 ios::sync_with_stdio(false);
 cin.tie(nullptr);
-cout<<"Hello Dev"<<endl;
+
+
+
+
+int n;
+cin>>n;
+pair<string,int> student[n];
+for (int i = 0; i < n; i++)
+{
+    cin>>student[i].first>>student[i].second;
+}
+for (auto [x,y]:student)
+{
+    cout<<y<<" "<<x<<endl;
+}
+
+
     return 0;
 }
