b9de1af3b498452fb1e7778366eaf0f9e139e3f4
md-hasan1 <akonhasan680@gmail.com>
2025-10-27 17:15:13 +0600

    vxczv

---
+++ P_Permutation_Minimization_by_Deque.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        deque<int> dq;
+        for (auto i = 0; i < n; i++)
+        {
+            int v;
+            cin >> v;
+            if(dq.empty()){
+                dq.push_back(v);
+            }else if (v>=dq.front())
+            {
+                
+            }
+            
+        }
+    }
+
+    return 0;
+}
